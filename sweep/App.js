f768b8463a35a7a76ac7f21334b97769232758bc
Rohan Pandey <rohan@Rohans-MacBook-Pro.local>
2018-09-08 13:01:37 -0700

    map

--- sweep/App.js
+++ sweep/App.js
@@ -1,22 +1,10 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
 import Map from './Views/Map';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Map />
-      </View>
+      <Map />
     );
   }
 }
--- sweep/Views/Map.js
+++ sweep/Views/Map.js
@@ -1,31 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import MapView from 'react-native-maps';
+// import { StyleSheet, View } from 'react-native';
+import { MapView } from 'expo';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
 
 export default class Map extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        {/* <Text> sWEep </Text> */}
-        {/* <Text> 我很聪明 </Text> */}
-        <MapView
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-        />
-      </View>
+      <MapView
+        style={{
+          flex: 1,
+        }}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     );
   }
 }
+
+/*
+  Keystore password: 3828eb7f0d3e1fa93af7c1a719159e70
+  Key alias:         QGtob29tZWlrL3NXRWVwLXNsdWc=
+  Key password:      e6095478947eb19ae54f6a32257dce15
+*/
