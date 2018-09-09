124ccb84702e45cf2a8ed16f8f37ba2ef34439db
PandawanFr <pandawanfr@gmail.com>
2018-09-08 22:02:03 -0700

    Vision, now recognizes trash (takes a while)

--- sweep/Views/Camera.js
+++ sweep/Views/Camera.js
@@ -1,29 +1,47 @@
 import React from 'react';
 import {
-	StyleSheet, Text, View, TouchableOpacity,
+	StyleSheet, Text, View,
 } from 'react-native';
 import { Camera, Permissions } from 'expo';
+import Clarifai from 'clarifai';
+
+import RoundedButton from '../Components/RoundedButton';
 
 const styles = StyleSheet.create({
 	basicFlex: {
 		flex: 1,
 	},
+	centerContent: {
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 	innerCameraView: {
 		flex: 1,
 		backgroundColor: 'transparent',
 		flexDirection: 'row',
-	},
-	cameraTouchableOpacity: {
-		flex: 0.1,
-		alignSelf: 'flex-end',
 		alignItems: 'center',
+		justifyContent: 'center',
 	},
 	cameraInfoText: {
 		fontSize: 18,
 		marginBottom: 10,
 		color: 'white',
 	},
+	takePhotoButton: {
+		bottom: 16,
+	},
+	closeCameraButton: {
+		backgroundColor: 'transparent',
+		top: 16,
+		right: 0,
+	},
 });
+
+
+const clarifai = new Clarifai.App({
+	apiKey: '37935bb2b4954757849cdc26d82a3386',
+});
+process.nextTick = setImmediate;
 
 export default class CameraView extends React.Component {
 	constructor(props) {
@@ -31,12 +49,48 @@ export default class CameraView extends React.Component {
 		this.state = {
 			hasCameraPermission: null,
 			type: Camera.Constants.Type.back,
+			isLoading: false,
 		};
 	}
 
 	async componentWillMount() {
 		const { status } = await Permissions.askAsync(Permissions.CAMERA);
 		this.setState({ hasCameraPermission: status === 'granted' });
+	}
+
+	pictureResults(data) {
+		console.log(data.type === 'success' ? (data.data.type === 'trash' ? 'TRASH!' : 'NOT TRASH!') : `Something went wrong... ${data.data}`);
+		this.setState({ isLoading: false });
+	}
+
+	async snap() {
+		if (this.camera) {
+			this.setState({ isLoading: true });
+			const photo = await this.camera.takePictureAsync({ base64: true });
+
+			try {
+				const response = await clarifai.models.predict(Clarifai.GENERAL_MODEL, photo.base64);
+				console.log(response);
+				const { concepts } = response.outputs[0].data;
+				let done = false;
+				if (concepts && concepts.length > 0) {
+					concepts.forEach((prediction) => {
+						// If already done, don't do it again
+						if (done) return;
+						console.log(`${prediction.name} ${prediction.value}`);
+						if ((prediction.name === 'cardboard' || prediction.name === 'glass' || prediction.name === 'metal' || prediction.name === 'paper' || prediction.name === 'plastic' || prediction.name === 'trash') && prediction.value >= 0.60) {
+							this.pictureResults({ type: 'success', data: { type: 'trash', image: photo } });
+							done = true;
+						}
+					});
+				}
+				if (!done) {
+					this.pictureResults({ type: 'success', data: { type: 'none', image: photo } });
+				}
+			} catch (error) {
+				this.pictureResults({ type: 'error', data: error });
+			}
+		}
 	}
 
 	render() {
@@ -46,24 +100,34 @@ export default class CameraView extends React.Component {
 		} if (hasCameraPermission === false) {
 			return <Text>No access to camera</Text>;
 		}
+		if (this.state.isLoading) {
+			return (
+				<View style={[styles.basicFlex, styles.centerContent]}>
+					<Text>Loading...</Text>
+				</View>
+			);
+		}
 		return (
 			<View style={styles.basicFlex}>
-				<Camera style={styles.basicFlex} type={this.state.type}>
+				<Camera
+					style={styles.basicFlex}
+					type={this.state.type}
+					ref={(ref) => { this.camera = ref; }}
+				>
 					<View style={styles.innerCameraView}>
-						<TouchableOpacity
-							style={styles.cameraTouchableOpacity}
+						<RoundedButton
+							style={styles.closeCameraButton}
 							onPress={() => {
-								this.setState(prevState => ({
-									type: prevState.type === Camera.Constants.Type.back
-										? Camera.Constants.Type.front
-										: Camera.Constants.Type.back,
-								}));
+								this.props.navigation.navigate('Map');
 							}}
-						>
-							<Text style={styles.cameraInfoText}>
-								{' '}Flip{' '}
-							</Text>
-						</TouchableOpacity>
+							icon="close"
+							underlayColor="transparent"
+						/>
+						<RoundedButton
+							style={styles.takePhotoButton}
+							onPress={async () => this.snap()}
+							icon="camera"
+						/>
 					</View>
 				</Camera>
 			</View>
